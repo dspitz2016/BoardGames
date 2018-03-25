655cd758f6772bf0caeafa08404ee2ac457f1b15
Neil Hiranandani <neilh578@gmail.com>
2018-03-25 13:30:27 -0400

    fixed missing brackets in comment service

--- express/services/comment.service.js
+++ express/services/comment.service.js
@@ -3,11 +3,11 @@ _this = this;
 // to create new comments based on inputed data
 exports.createComment = async function(c) {
   // new object to insert in database
-  var newComment = new Comment(
-    id:_this.c.id,
-    name:_this.c.name,
-    comment:_this.c.comment
-  )};
+  var newComment = new Comment({
+    id: _this.c.id,
+    name: _this.c.name,
+    comment: _this.c.comment
+  });
 // try to insert into database, error if can't
   try {
     var nCom = await newComment.save();
@@ -21,7 +21,7 @@ exports.createComment = async function(c) {
 // to get comments from database based on game ID number
 exports.getComments = async function(game) {
    try {
-      var com = await Comment.find(game_id: game.game_id); 
+      var com = await Comment.find({game_id: game.game_id}); 
    }
   catch(err) {
     throw Error('Error finding comments with this ID'); 
