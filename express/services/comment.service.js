d69644f2d3550b786f723fc46b35b8c87244974f
bmm723 <bmm7238@rit.edu>
2018-03-25 13:16:25 -0400

    Update comment.service.js

--- express/services/comment.service.js
+++ express/services/comment.service.js
@@ -1,13 +1,15 @@
 var Comment = require('../models/comment.model');
 _this = this;
 
+// to create new comments based on inputed data
 exports.createComment = async function(c) {
-  
+  // new object to insert in database
   var newComment = new Comment(
     id:c.id,
     name:c.name,
     comment:c.comment
   )};
+// try to insert into database, error if can't
   try {
     var nCom = await newComment.save();
     return nCom;
@@ -17,7 +19,12 @@ exports.createComment = async function(c) {
   }
 }
 
-// to get comments from database based on ID number
-exports.getComments = async function(id) {
-   
+// to get comments from database based on game ID number
+exports.getComments = async function(game) {
+   try {
+      var com = await Comment.find(game_id: game.game_id); 
+   }
+  catch(err) {
+    throw Error('Error finding comments with this ID'); 
+  }
 }
