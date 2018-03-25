abce3dfea1d8dc281f857d45655ce0a13baea0e4
bmm723 <bmm7238@rit.edu>
2018-03-25 12:29:40 -0400

    Update comments.model.js

--- express/models/comments.model.js
+++ express/models/comments.model.js
@@ -1,7 +1,7 @@
 // mongoose schema for comments collection
 
 var mongoose = require('mongoose');
-var mongoosePag = require('mongoose-paginate');
+// var mongoosePag = require('mongoose-paginate');
 
 /* "game_id" is a unique value in each game in the database. Do we
 * want to use the "game_id" value as the identifier for comments
@@ -13,6 +13,8 @@ var CommentSchema = new mongoose.Schema({
   comment: String
 });
 
-CommentSchema.plugin(mongoosePag);
-const Comment = mongoose.model('Comment', CommentSchema);
+// CommentSchema.plugin(mongoosePag);
+// const Comment = mongoose.model('Comment', CommentSchema);
+
+var Comment = mongoose.model('Comment', CommentSchema)
 module.exports = Comment;
