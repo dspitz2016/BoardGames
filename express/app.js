cb396dfd66f3dfcd834c274a7ca724c95821597d
Neil Hiranandani <neilh578@gmail.com>
2018-03-25 12:47:37 -0400

    mongoose set up in app.js

--- express/app.js
+++ express/app.js
@@ -7,7 +7,17 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var mongoose = require('mongoose');
+
 var app = express();
+
+//mongoose initialization
+var mongoDB = 'mongodb://127.0.0.1:27017/GameData';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB conneciton error');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
