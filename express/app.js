a5894d7299a064e590094d06cc438b1912acee39
Neil Hiranandani <neilh578@gmail.com>
2018-03-25 12:48:08 -0400

    fixed spelling error

--- express/app.js
+++ express/app.js
@@ -16,7 +16,7 @@ var mongoDB = 'mongodb://127.0.0.1:27017/GameData';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB conneciton error');
+db.on('error', console.error.bind(console, 'MongoDB connection error');
 
 
 // view engine setup
